634f3dbed1683c97b4c07f7f6a80729315cd63a3
Juan Palomino <jpalomino09@gmail.com>
2024-03-01 01:12:32 -0700

    main deet

--- pages/create/Broadcast.tsx
+++ pages/create/Broadcast.tsx
@@ -5,8 +5,10 @@
 import React, { useEffect, useState } from "react";
 import { Page, Nav } from "../../components";
 import { useCreateStream } from "@livepeer/react";
+import { Button } from "../../components/shared";
+import 'webrtc';
 
-//react functional component
+
 
 export default function Broadcast() {
 // populate the canvas element with the users webcam
@@ -32,19 +34,106 @@ export default function Broadcast() {
         });
     }, []);
 
-    // function to start live webtrc stream to livepeer
+    // function to start livestream to rtmp://rtmp.livepeer.com/live/4f79-ii4v-fcmf-pqyj
+
+    const startStream = async () => {
+    // streams canvas to rtmp://rtmp.livepeer.com/live/4f79-ii4v-fcmf-pqyj
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    const stream = canvas.captureStream(30);
+    const mediaRecorder = new MediaRecorder(stream);
+
+const redirectUrl = `https://lax-prod-catalyst-2.lp-playback.studio/webrtc/7450335d-bffa-4b38-82b9-ce0139ff4ff1`;
+// we use the host from the redirect URL in the ICE server configuration
+const host = new URL(redirectUrl).host;
+
+const iceServers = [
+  {
+    urls: `stun:${host}`,
+  },
+  {
+    urls: `turn:${host}`,
+    username: "livepeer",
+    credential: "livepeer",
+  },
+];
+
+// get user media from the browser (which are camera/audio sources)
+const mediaStream = stream
+const peerConnection = new RTCPeerConnection({ iceServers });
+
+
+
+const newVideoTrack = mediaStream?.getVideoTracks?.()?.[0] ?? null;
+const newAudioTrack = mediaStream?.getAudioTracks?.()?.[0] ?? null;
+
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createOffer
+ * We create an SDP offer here which will be shared with the server
+ */
+const offer = await peerConnection.createOffer();
+/** https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/setLocalDescription */
+await peerConnection.setLocalDescription(offer);
+console.log("establishing connection", offer);
+let ofr;
+/** Wait for ICE gathering to complete */
+ofr = await new Promise((resolve) => {
+  /** Wait at most five seconds for ICE gathering. */
+  setTimeout(() => {
+    resolve(peerConnection.localDescription);
+  }, 5000);
+  peerConnection.onicegatheringstatechange = (_ev) => {
+    if (peerConnection.iceGatheringState === "complete") {
+      resolve(peerConnection.localDescription);
+    }
+  };
+});
+if (!ofr) {
+  throw Error("failed to gather ICE candidates for offer");
+}
+/**
+ * This response contains the server's SDP offer.
+ * This specifies how the client should communicate,
+ * and what kind of media client and server have negotiated to exchange.
+ */
+
+
+const sdpResponse = await fetch(redirectUrl, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+        "content-type": "application/sdp",
+    },
+    //ignore ts error
+    // eslint-disable-next-line 
+    body: (ofr as { sdp: string }).sdp,
+});
+console.log("sdp",sdpResponse);
+if (sdpResponse.ok) {
+    const answerSDP = await sdpResponse.text();
+    await peerConnection.setRemoteDescription(
+        new RTCSessionDescription({ type: "answer", sdp: answerSDP })
+    );
+}
+
+};
+
 
   //returning the page component
   return (
     <Page>
       <Nav />
       <div>
-        <h1>Go Live</h1>
-        <p>Click the button below to go live</p>
         {/* html canvas element that is populated by users webcam */}
         <canvas id="canvas" width="640" height="480"></canvas>
         {/* button to start the live stream */}
-        <button>Go Live</button>
+        <Button
+                    className={`bg-primary border-primary text-background px-4 py-2.5`}
+                    text="text-sm"
+                    onClick={() => startStream()}
+                  >
+                    Start Stream
+     </Button>
       </div>
     </Page>
   );
